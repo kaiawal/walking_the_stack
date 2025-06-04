525a5db48365ba62d89a85ba0244808f1865ba4e
jmuy.333 <jmuyuela203@gmail.com>
2025-06-04 15:38:02 -0700

    dump registers

--- src/asm/dump_registers.s
+++ src/asm/dump_registers.s
@@ -2,8 +2,24 @@
 .globl dump_registers
 .type dump_registers, @function
 dump_registers:
-subq $(16*8), %rsp
-movq %rsp, %rdi
-call _debug_dump_registers
-addq $(16*8), %rsp
-ret
+    subq $(16*8), %rsp
+    movq %rsp, %rdi
+    push %rax
+    push %rbx
+    push %rcx
+    push %rdx
+    push %rsi
+    push %rdi
+    push %rbp
+    push %rsp
+    push %r8
+    push %r9
+    push %r10
+    push %r11
+    push %r12
+    push %r13
+    push %r14
+    push %r15
+    call _debug_dump_registers
+    addq $(16*8), %rsp
+    ret
